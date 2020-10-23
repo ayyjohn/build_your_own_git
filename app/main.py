0c5295e41fd1b6e4acada8373cd4b23465622d50
Alec Johnson <alecjohnson55@gmail.com>
2020-10-22 21:54:01 -0700

    add handling for basic cat-file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import zlib
 
 
 def main():
@@ -12,6 +13,14 @@ def main():
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
     elif command == "cat-file":
+        # argv[2] will always be "-p" for now
+        blob_sha = sys.argv[3]
+        # first 2 chars are directory
+        dirname, filename = blob_sha[:2], blob_sha[2:]
+        with open(f".git/objects/{dirname}/{filename}", "rb") as f:
+            # git cat-file -p 0a5e5a61944bc3228ba609690585a9b52b1e31b7
+            print(zlib.decompress(f.read()), end="")
+
         print("dumpty scooby donkey dumpty vanilla monkey", end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
