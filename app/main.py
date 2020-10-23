b3134d44bbd091cecc3d1bbdc1c0d67f80717d91
Alec Johnson <alecjohnson55@gmail.com>
2020-10-23 10:39:47 -0700

    refactor hash file and data into helper methods

--- app/main.py
+++ app/main.py
@@ -34,25 +34,35 @@ def main():
     elif command == "hash-object":
         # format is `git hash-object -w filename`
         # thus argv[2] will always be "-w" for now
-        file_name = sys.argv[3]
-        with open(file_name, "rb") as f:
-            data = f.read()
-            contents = b"blob " + str(len(data)).encode(UTF8) + NULL + data
-            sha = hashlib.sha1()
-            sha.update(contents)
-            blob_hash = sha.hexdigest()
-            dirname, filename = get_dir_and_file_names_from_hash(blob_hash)
-            object_dir = Path(f"{OBJECTS_DIR}/{dirname}")
-            object_dir.mkdir(exist_ok=True)
-            with open(f"{OBJECTS_DIR}/{dirname}/{filename}", "wb") as blob:
-                blob.write(zlib.compress(contents))
-            print(blob_hash, end="")
+        input_file_name = sys.argv[3]
+        blob_hash = hash_file(input_file_name)
+        dirname, output_file_name = get_dir_and_file_names_from_hash(blob_hash)
+        object_dir = Path(f"{OBJECTS_DIR}/{dirname}")
+        object_dir.mkdir(exist_ok=True)
+        with open(f"{OBJECTS_DIR}/{dirname}/{output_file_name}", "wb") as blob:
+            blob.write(zlib.compress(contents))
+        print(blob_hash, end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
 
 def decode(b):
     return b.decode(UTF8)
+
+
+def hash_file(file_name):
+    with open(file_name, "rb") as f:
+        data = f.read()
+        # format of file contents are `blob <filelength>\x00<filedata>`
+        contents = b"blob " + str(len(data)).encode(UTF8) + NULL + data
+        blob_hash = hash_data(contents)
+    return blob_hash
+
+
+def hash_data(data):
+    sha = hashlib.sha1()
+    sha.update(data)
+    return sha.hexdigest()
 
 
 def get_dir_and_file_names_from_hash(blob_hash):
