294f238bd59cd86d14885b5810bfee8d49a59ff2
Alec Johnson <alecjohnson55@gmail.com>
2020-11-09 18:53:05 -0800

    add echo server for testing commands

---
+++ echo_server.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from io import BytesIO
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello, world!")
+
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(("localhost", 3000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
